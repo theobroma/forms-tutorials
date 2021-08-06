08d504baf01851bd169b9a96b139103683025246
Alexandr Siryj <theobroma222@gmail.com>
2021-08-07 00:41:36 +0300

    step-4 managingform state

--- src/@components/YoutubeForm/YoutubeForm.tsx
+++ src/@components/YoutubeForm/YoutubeForm.tsx
@@ -1,20 +1,55 @@
 import React from 'react';
 import { useFormik } from 'formik';
 
+interface IFormFields {
+  name: string;
+  email: string;
+  channel: string;
+}
+
 const YoutubeForm: React.FC = () => {
-  //   const formik = useFormik({});
+  const initialValues: IFormFields = {
+    name: 'Vishwas',
+    email: '',
+    channel: '',
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      console.log('submit:', values);
+    },
+  });
+
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
-
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
