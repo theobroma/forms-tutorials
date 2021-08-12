787c8d446c4e48a145c16edf8119a495c04b2c22
Alexandr Siryj <theobroma222@gmail.com>
2021-08-12 03:10:39 +0300

    start formik-controls-demo

---
+++ formik-controls-demo/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './input.css';
+import YoutubeForm from './@components/YoutubeForm';
+
+function App() {
+  return (
+    <div className="App">
+      <YoutubeForm />
+    </div>
+  );
+}
+
+export default App;
