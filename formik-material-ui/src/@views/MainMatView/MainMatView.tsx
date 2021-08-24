4db7f2628dfdf5ce6c1c77bb0bacec9b560f8947
Alexandr Siryj <theobroma222@gmail.com>
2021-08-24 21:38:27 +0300

    MUI RegistrationForm

--- formik-material-ui/src/@views/MainMatView/MainMatView.tsx
+++ formik-material-ui/src/@views/MainMatView/MainMatView.tsx
@@ -2,6 +2,7 @@ import { Box, Container, Grid } from '@material-ui/core';
 import React from 'react';
 import Footer from '../../@components/Layout/Footer';
 import PrimaryAppBar from '../../@components/Layout/PrimaryAppBar';
+import RegistrationForm from '../../@composites/RegistrationForm';
 
 const MainMatView: React.FC = () => {
   return (
@@ -13,7 +14,7 @@ const MainMatView: React.FC = () => {
         <Container maxWidth="lg">
           <Grid container spacing={3} style={{ padding: 3 }}>
             <Grid item xs={12}>
-              MainMatView
+              <RegistrationForm />
             </Grid>
           </Grid>
         </Container>
