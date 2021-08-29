20cba22f842ef6aa6b51fe39d698eb8a494a875a
Alexandr Siryj <theobroma222@gmail.com>
2021-08-29 22:46:47 +0300

    cleanup formik-react-bootstrap folder

--- formik-react-bootstrap/src/@views/MainBootstrapView/MainBootstrapView.tsx
+++ formik-react-bootstrap/src/@views/MainBootstrapView/MainBootstrapView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Alert } from 'react-bootstrap';
+// import LoginForm from '../../@composites/LoginForm';
 
 const MainBootstrapView: React.FC = () => {
   return (
@@ -11,22 +12,25 @@ const MainBootstrapView: React.FC = () => {
             <Col xs={12} md={{ span: 6, offset: 3 }}>
               <h3>Login Page</h3>
               <Alert variant="success" className="mb-5">
-                <p>Данные тестового аккаунта:</p>
+                {/* <p>Данные тестового аккаунта:</p>
                 <hr />
                 <p>Email: free@samuraijs.com</p>
-                <p className="mb-0">Password: free</p>
+                <p className="mb-0">Password: free</p> */}
+                <p>use form filler</p>
               </Alert>
-              form
-              {/* <LoginForm
-                submitCallback={submitCallback}
-                captchaUrl={captchaUrl}
-              /> */}
+              {/* <LoginForm /> */}
             </Col>
           </Row>
         </Container>
       </div>
       {/* <Footer /> */}
-      <footer>footer</footer>
+      <footer style={{ backgroundColor: '#cce5ff' }}>
+        <Container>
+          <Row>
+            <Col xs={12}>footer</Col>
+          </Row>
+        </Container>
+      </footer>
     </div>
   );
 };
--- formik-react-bootstrap/src/App.tsx
+++ formik-react-bootstrap/src/App.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-// import EnrollmentFormView from './@views/EnrollmentFormView';
-// import FormikContainerView from './@views/FormikContainerView';
-// import LoginFormView from './@views/LoginFormView';
-// import RegistrationFormView from './@views/RegistrationFormView';
-import './App.css';
-import './input.css';
-
 // All styles
 import './@assets/styles/index.scss';
 import MainBootstrapView from './@views/MainBootstrapView';
 
 function App() {
-  return (
-    // <div className="App">
-    //   {/* uncomment one view */}
-    //   <FormikContainerView />
-    //   <LoginFormView />
-    //   <RegistrationFormView />
-    //   <EnrollmentFormView />
-    //   <MatLoginFormViews />
-    // </div>
-    <MainBootstrapView />
-  );
+  return <MainBootstrapView />;
 }
 
 export default App;
