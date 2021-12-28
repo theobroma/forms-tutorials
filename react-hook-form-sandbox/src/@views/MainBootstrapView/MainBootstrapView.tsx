c52df372390ef1d37fe4e5028ad4d3ff211d94df
Alexandr Siryj <theobroma222@gmail.com>
2021-12-28 20:12:35 +0200

    RHF BasicExample

--- react-hook-form-sandbox/src/@views/MainBootstrapView/MainBootstrapView.tsx
+++ react-hook-form-sandbox/src/@views/MainBootstrapView/MainBootstrapView.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import BasicExample from '../../@components/BasicExample/BasicExample';
 
 const MainBootstrapView: React.FC = () => {
   return (
     <div className="HolyGrail">
       <h2>bootstrap</h2>
-      <div className="HolyGrail-content">content</div>
+      <div className="HolyGrail-content">
+        <BasicExample />
+      </div>
       {/* <Footer /> */}
       <footer style={{ backgroundColor: '#cce5ff' }}>footer</footer>
     </div>
