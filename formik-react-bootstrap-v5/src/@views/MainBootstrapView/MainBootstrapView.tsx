2ad1a1f46a48f9e99f3da938cba7b66b65509373
Alexandr Siryj <theobroma222@gmail.com>
2021-12-28 12:56:50 +0200

    formik-react-bootstrap-v5

---
+++ formik-react-bootstrap-v5/src/@views/MainBootstrapView/MainBootstrapView.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
+import LoginForm from '../../@composites/LoginForm';
+import SamuraiLoginForm, {
+  LoginPayload,
+} from '../../@composites/SamuraiLoginForm/SamuraiLoginForm';
+
+const MainBootstrapView: React.FC = () => {
+  const submitCallback = (payload: LoginPayload) => {
+    console.log(payload);
+  };
+
+  return (
+    <div className="HolyGrail">
+      <h2>bootstrap</h2>
+      <div className="HolyGrail-content">
+        <Container>
+          <Row className="mt-3">
+            <Col xs={12} md={{ span: 6, offset: 3 }}>
+              <h3>Login Page</h3>
+              <Alert variant="success" className="mb-5">
+                <p>use form filler</p>
+              </Alert>
+              {/* UNCOMMENT ONE */}
+              <SamuraiLoginForm
+                submitCallback={submitCallback}
+                captchaUrl={null}
+              />
+              {/* <LoginForm /> */}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      {/* <Footer /> */}
+      <footer style={{ backgroundColor: '#cce5ff' }}>
+        <Container>
+          <Row>
+            <Col xs={12}>footer</Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
+};
+
+export default MainBootstrapView;
---
+++ formik-react-bootstrap-v5/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// All styles
+import './@assets/styles/index.scss';
+import MainBootstrapView from './@views/MainBootstrapView';
+
+const App = () => {
+  return <MainBootstrapView />;
+};
+
+export default App;
