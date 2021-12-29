94b28f3041f098ea581d840d5616be7f24c4e8ff
Alexandr Siryj <theobroma222@gmail.com>
2021-12-29 14:58:51 +0200

    YupBasicExample

--- react-hook-form-sandbox/src/@views/MainBootstrapView/MainBootstrapView.tsx
+++ react-hook-form-sandbox/src/@views/MainBootstrapView/MainBootstrapView.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BasicExample from '../../@components/BasicExample/BasicExample';
+import YupBasicExample from '../../@components/YupBasicExample/YupBasicExample';
 
 const MainBootstrapView: React.FC = () => {
   return (
     <div className="HolyGrail">
       <h2>bootstrap</h2>
       <div className="HolyGrail-content">
-        <BasicExample />
+        {/* <BasicExample /> */}
+        <YupBasicExample />
       </div>
       {/* <Footer /> */}
       <footer style={{ backgroundColor: '#cce5ff' }}>footer</footer>
