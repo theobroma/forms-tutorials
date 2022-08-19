349860de29541192b5be9d82cb4cc75fd7bb9b1a
Alexandr Siryj <theobroma222@gmail.com>
2022-08-20 00:32:46 +0300

    fix useForm type for rhf-sandbox

--- rhf-sandbox/src/@components/ZodBasicExample/ZodBasicExample.tsx
+++ rhf-sandbox/src/@components/ZodBasicExample/ZodBasicExample.tsx
@@ -1,3 +1,4 @@
+// https://github.com/react-hook-form/react-hook-form/issues/8805
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
@@ -8,29 +9,24 @@ const schema = z.object({
   age: z.number().min(10),
 });
 
+type SchemaType = z.infer<typeof schema>;
+
 const ZodBasicExample = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
+  } = useForm<SchemaType>({
+    // defaultValues: { name: 'name', age: 11 },
     resolver: zodResolver(schema),
   });
 
   return (
     <form onSubmit={handleSubmit((d) => console.log(d))}>
       <input {...register('name')} />
-      {!!errors.name?.message && (
-        <p>
-          <>{errors.name?.message}</>
-        </p>
-      )}
+      {!!errors.name?.message && <p>{errors.name?.message}</p>}
       <input type="number" {...register('age', { valueAsNumber: true })} />
-      {!!errors.age?.message && (
-        <p>
-          <>{errors.age?.message}</>
-        </p>
-      )}
+      {!!errors.age?.message && <p>{errors.age?.message}</p>}
       <input type="submit" />
     </form>
   );
