13d92db579e1159c96cf5e50a6feba94246dde55
Alexandr Siryj <theobroma222@gmail.com>
2022-08-20 04:26:08 +0300

    MediumSignupForm

--- rhf-mui-v5/src/@views/MainView/MainView.tsx
+++ rhf-mui-v5/src/@views/MainView/MainView.tsx
@@ -1,15 +1,17 @@
 // import BasicExample from '../../@components/BasicExample/BasicExample';
 // import YupBasicExample from '../../@components/YupBasicExample/YupBasicExample';
-import ZodBasicExample from '../../@components/ZodBasicExample/ZodBasicExample';
+// import ZodBasicExample from '../../@components/ZodBasicExample/ZodBasicExample';
+import MediumSignupForm from '../../@components/MediumSignup/MediumSignup';
 
 const MainView = () => {
   return (
     <div className="HolyGrail">
-      <h2>bootstrap</h2>
+      <h2>mui v5</h2>
       <div className="HolyGrail-content">
         {/* <BasicExample /> */}
         {/* <YupBasicExample /> */}
-        <ZodBasicExample />
+        {/* <ZodBasicExample /> */}
+        <MediumSignupForm />
       </div>
       {/* <Footer /> */}
       <footer style={{ backgroundColor: '#cce5ff' }}>footer</footer>
