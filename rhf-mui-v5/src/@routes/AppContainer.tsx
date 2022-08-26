aab063d1e07a74d98ad82b45f6ea168e3f38db4d
Alexandr Siryj <theobroma222@gmail.com>
2022-08-27 02:27:32 +0300

    resolver  MediaTabs

--- rhf-mui-v5/src/@routes/AppContainer.tsx
+++ rhf-mui-v5/src/@routes/AppContainer.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import BasicExample from '../@components/ResolverExamples/BasicExample/BasicExample';
+import YupBasicExample from '../@components/ResolverExamples/YupBasicExample/YupBasicExample';
+import ZodBasicExample from '../@components/ResolverExamples/ZodBasicExample/ZodBasicExample';
 import LogRocketView from '../@views/LogRocketView';
 import MainView from '../@views/MainView';
 import MediumSignupView from '../@views/MediumSignupView';
-import ResolverView from '../@views/ResolverView';
+import ResolverLayout from '../@views/ResolverView/ResolverLayout';
 import SocialFormsView from '../@views/SocialFormsView';
 import StrictTypingView from '../@views/StrictTypingView';
 
@@ -15,7 +18,13 @@ const AppContainer = () => {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<MainView />} />
-          <Route path="resolvers" element={<ResolverView />} />
+          {/* resolvers */}
+          <Route path="resolvers" element={<ResolverLayout />}>
+            <Route path="basic" element={<BasicExample />} />
+            <Route path="yup" element={<YupBasicExample />} />
+            <Route path="zod" element={<ZodBasicExample />} />
+            <Route index element={<div>Click any tab.</div>} />
+          </Route>
           <Route path="logrocket" element={<LogRocketView />} />
           <Route path="medium" element={<MediumSignupView />} />
           <Route path="strict-typing" element={<StrictTypingView />} />
