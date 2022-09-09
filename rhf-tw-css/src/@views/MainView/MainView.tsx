e7c95c518379257823699a0248d88c8035c05178
Alexandr Siryj <theobroma222@gmail.com>
2022-09-09 05:14:33 +0300

    add flowbite

--- rhf-tw-css/src/@views/MainView/MainView.tsx
+++ rhf-tw-css/src/@views/MainView/MainView.tsx
@@ -1,8 +1,16 @@
+import { Alert } from 'flowbite-react';
+
 import JobForm from '../../@components/JobForm';
 
 const MainView = () => {
   return (
     <div>
+      <Alert color="info">
+        <span>
+          <span className="font-medium">Info alert!</span> Flowbite connected
+          !!!
+        </span>
+      </Alert>
       <JobForm />
     </div>
   );
