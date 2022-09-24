382c6733dd84344fce7fbb334672f634ed8555b9
Alexandr Siryj <theobroma222@gmail.com>
2022-09-24 13:33:31 +0300

    @semantic-ui-react/css-patch

--- rhf-semantic-ui/src/index.tsx
+++ rhf-semantic-ui/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
-// import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
