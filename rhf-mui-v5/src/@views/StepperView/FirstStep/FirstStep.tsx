5f5627e80a108355f2d1a0e2d6c3d6ae27fbe4c1
Alexandr Siryj <theobroma222@gmail.com>
2022-10-13 22:25:44 +0300

    step1 preperation

--- rhf-mui-v5/src/@views/StepperView/FirstStep/FirstStep.tsx
+++ rhf-mui-v5/src/@views/StepperView/FirstStep/FirstStep.tsx
@@ -1,8 +1,30 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useStyles } from 'tss-react/mui';
+
 type Props = {
   title: string;
 };
 
 const FirstStep = ({ title }: Props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  // const [initialValues] = useState(getFormData());
+
+  const styles = useStyles();
+
+  // const onSubmit = useCallback(
+  //   (values) => {
+  //     setFormData(values);
+  //     history.push({
+  //       ...location,
+  //       state: {
+  //         activeStep: 1,
+  //       },
+  //     });
+  //   },
+  //   [history, location],
+  // );
+
   return (
     <div>
       <h2>{title}</h2>
--- rhf-mui-v5/src/@views/StepperView/StepperView.tsx
+++ rhf-mui-v5/src/@views/StepperView/StepperView.tsx
@@ -1,26 +1,56 @@
-import * as React from 'react';
+// https://stackoverflow.com/questions/64566405/react-router-dom-v6-usenavigate-passing-value-to-another-component
+
+import { useLocation } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Stepper from '@mui/material/Stepper';
 
-const steps = [
-  'Select master blaster campaign settings',
-  'Create an ad group',
-  'Create an ad',
+import FirstStep from './FirstStep';
+import SecondStep from './SecondStep';
+import ThirdStep from './ThirdStep';
+
+const TABS = [
+  {
+    component: FirstStep,
+    title: 'First Step',
+  },
+  {
+    component: SecondStep,
+    title: 'Second Step',
+  },
+  {
+    component: ThirdStep,
+    title: 'Review',
+  },
 ];
 
 const StepperView = () => {
+  //   const { state = { activeStep: 0 } } = useLocation();
+  const location = useLocation();
+  const { state = { activeStep: 0 } } = location;
+
+  //   console.log('activeStep :>> ', state.activeStep);
+
+  //   const tab = TABS[state.activeStep];
+  const tab = TABS[0];
+
   return (
     <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={1} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
+      <Stepper
+        //   activeStep={1}
+        activeStep={0}
+        // activeStep={state.activeStep}
+        alternativeLabel
+      >
+        {TABS.map(({ title }) => (
+          <Step key={title}>
+            <StepLabel>{title}</StepLabel>
           </Step>
         ))}
       </Stepper>
+      {!!tab && <tab.component title={tab.title} />}
     </Box>
   );
 };
